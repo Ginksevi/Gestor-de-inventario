47822c308da5dfefecdedb1be0c388a3d8a7f880
Gian <gianseveriche97@gmail.com>
2026-07-17 21:21:59 -0500

    un cambio menor de nombre a la variable 'id' para evitar confuciones o posibles errores con la funcion navita de python con el mismo nombre

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from extension import db
 
 class Producto(db.Model): # db.Model comvierte la clase en una tabla
-    id = db.Column(db.Integer, primary_key = True) # Define la clase primaria
+    id_key = db.Column(db.Integer, primary_key = True) # Define la clase primaria
     nombre = db.Column(db.String(100), nullable = False) # nullable=False, Obliga a que el campo tenga valor evitando datos incompletos
     cantidad = db.Column(db.Integer, default = 0) # default=0 asegura que la cantidad nunca sea NULL
     precio = db.Column(db.Float, nullable = False)
