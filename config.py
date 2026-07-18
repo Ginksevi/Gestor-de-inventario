fecf451b004b2deb9cf785f7f60a81012f429ba4
Gian <gianseveriche97@gmail.com>
2026-07-17 22:05:21 -0500

    cambios menores en la variable DATABASE_URI

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:tucontraseñasegura@localhost:3306/inventario' # Debes cambiar "flaskuser" por tu usuario de MySQL y la contraseña
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://FlaskUser:TuContraseñaSegura@localhost/inventario' # Debes cambiar "flaskuser" por tu usuario de MySQL y la contraseña
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'clave secreta'
