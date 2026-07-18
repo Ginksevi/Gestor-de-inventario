aced36d71aac9efaa2e9e89192becf00db07ee87
Gian <gianseveriche97@gmail.com>
2026-07-17 22:01:58 -0500

    correción en la función 'editar', hizo falta añadirle un return render_template() ya que daba error al no tener redireccion

--- app.py
+++ app.py
@@ -27,5 +27,22 @@ def agregar():
     db.session.commit() # Guarda los cambios en MySQL
     return redirect(url_for('index')) # Redirige para evitar reenvios de formularios
 
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    producto = Producto.query.get_or_404(id)
+    db.session.delete(producto)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>', methods = ['GET', 'POST'])
+def editar(id):
+    producto = Producto.query.get_or_404(id)
+    if request.method == 'POST':
+        producto.nombre = request.form['nombre']
+        producto.cantidad = int(request.form['cantidad'])
+        producto.precio = float(request.form['precio'])
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editar.html', producto = producto)
 if __name__ == '__main__':
     app.run(debug=True)
